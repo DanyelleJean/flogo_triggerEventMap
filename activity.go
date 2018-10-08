7a7716c5dd0b782812e2a78af4d7ecd0d88fda31
Danyelle Jean <dmiller1084@gmail.com>
2018-10-08 15:59:31 -0600

    initial commit

---
+++ activity.go
@@ -0,0 +1,52 @@
+package triggerScribeEventMap
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"net/http"
+	"strings"
+)
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
+	strMapUrl = context.GetInput("mapUrl").(string)
+	strValueList := context.GetInput("valueList").(string)
+	values := strings.Split(strValueList, ",")
+	var responses [len(values)]object
+
+	for index; index < len(values);index++  {
+		strRequestPayload := map[string]interface{}{"value":strValueList,}
+		payloadAsBytes, error := json.Marshal(strRequestPayload)
+
+		if error != nil {
+			return true, error
+		}
+
+		response, requestError := http.Post(strMapUrl, "application/json", bytes.NewBuffer(payloadAsBytes))
+
+		if requestError != nil {
+			return true, requestError
+		}
+
+		responses[index] = response
+	}
+
+	context.SetOutput("output", responses)
+	return true, nil
+}
